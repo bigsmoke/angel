73cf4a0ed43b0ad7954288ffb33b4ed520e35a7f
Rowan Van der Molen <rowan@ytec.nl>
2018-03-30 17:37:39 +0200

    Messing around in C++ BULL parser

--- BULLParser.hh
+++ BULLParser.hh
@@ -6,219 +6,113 @@ namespace Angel
 {
     namespace BULL
     {
-        /*
-        class Indicator
+        /**
+         * An angel is an _endpoint_ of a step (left or right in BULL).
+         *
+         * Angels must either exist as part of a path, be themselves a path, or be a subpath.
+         *
+         * Conceptually, an angel is not an object, although, in C++, it can only be represented as an object.
+         * How much an angel is not an objects is exampflified by the fact that it's always a placeholder
+         * 
+         *    * An angel either
+         */
+        class Angel
         {
             public:
-                char indicator() = 0;
-                bool canBeHeavy() = 0;
-                bool isHeavy() = 0;
-                bool isStep() = 0;
+                bool isUnknown();
+                bool isPath();
+
+            protected:
+                bool isUnknown;
+                bool isPath;
+                bool 
         };
 
-        class Step : public Indicator
+        class Angels
         {
+        };
+
+        /**
+         * A placeholder can represent any number of angels.
+         */
+        class Placeholder
+        {
+            enum class Type
+            {
+                UnknownAngel = (1 << 0),
+                NamedAngel = (1 << 1),
+                AngelDefinition = (1 << 2),
+                UnknownAngelDefinition = (1 << 0) | (1 << 2),
+                NamedAngelDefinition = (1 << 1) | (1 << 2),
+                LeftmostSubstring = (1 << 3),
+                Substring = (1 << 4),
+                SubstringCount = (1 << 5),
+                SubsequentSubstringCount = (1 << 6),
+                CallingContext = (1 << 7),
+                SuperpathContext = (1 << 8),
+                CommandArgument = (1 << 9),
+                AngelEvent = (1 << 10),
+            };
+
             public:
-                bool isStep() { return true; }; 
-        };
+                Placeholder(const Type type) : _type(type) {};
+                Placeholder(const Type type, const int substringPosition)
+                    : _type(type), _substringPosition(substringPosition) {};
 
-        class StepLeft : public Indicator
-        {
-            public
-                char indicator() { return '\\'; };
-                bool canBeHeavy() { return true; };
-                bool isHeavy();
+                bool hasType(const Type type) {
+                    return static_cast<int>(_type) & static_cast<int>(type) > 0;
+                }
+
+                int substringPosition(void)
+                {
+                    return _substringPosition;
+                }
 
             private:
-                bool heavy;
+                Type _type
+                int _substringPosition = 0;
+
         };
 
-        class StepRight : public Indicator
+        class Step
         {
-            public
-                char indicator() { return '/'; };
-                bool canBeHeavy() { return true; };
-                bool isHeavy();
+            protected:
+                Placeholder* stepTarget;
+        };
+
+        class Point
+        {
+            protected:
+                Step* fromStep;
+                Step* toStep;
+                bool pointsBack;
+                Placeholder* relationship;
+        };
+
+        class Path
+        {
+            public:
+                /**
+                 * Jog along the steps of a path and return the imprint left by the heavy steps.
+                 */
+                Angels jog();       
 
             private:
-                bool heavy;
+                std::set<Step> steps;
         };
-
-        class StepUp : public Indicator
-        {
-            public:
-                char indicator() { return '|'; };
-                bool canBeHeavy() { return false; };
-                bool isHeavy() { return false; };
-        }
-
-        class Point : public Indicator
-        {
-            public:
-                bool canBeHeavy() { return false; };               
-                bool isHeavy() { return false; };               
-                bool isStep() { return false; };
-        }
-
-        class PointLeft : public Point
-        {
-            public:
-                char indicator() { return '<'; };
-        }
-
-        class PointRight : public Point
-        {
-            public:
-                char indicator() { return '>'; };
-        }
-
-        class Super : public Indicator
-        {
-            public:
-                char indicator() { return '^'; };
-                bool canBeHeavy() { return false; };               
-                bool isHeavy() { return false; };               
-                bool isStep() { return false; };
-        }
-
-        class Length : public Indicator
-        {
-            public:
-                char indicator() { return '#'; };
-                bool canBeHeavy() { return false; };               
-                bool isHeavy() { return false; };               
-                bool isStep() { return false; };
-        };
-
-        class Knot : public Indicator
-        {
-            public:
-                char indicator() { return '&'; };
-                bool canBeHeavy() { return false; };               
-                bool isHeavy() { return false; };               
-                bool isStep() { return false; };
-                PointLeft& left;
-                PointRight& right;
-        }
-
-        class Unspecified : public Indicator
-        {
-            public:
-                char indicator() { return '?'; };
-                bool canBeHeavy() { return true; };
-                bool isHeavy();
-                bool isStep() { return false; };
-
-            private:
-                bool heavy;
-        };
-
-        class Creation : public Indicator
-        {
-            public:
-                char indicator() { return '+'; };
-                bool canBeHeavy() { return false; };
-                bool isHeavy() { return false; };
-                bool isStep() { return false; };
-        };
-
-        class Argument : public Indicator
-        {
-            public:
-                char indicator() { return '@'; };
-                bool canBeHeavy() { return false; };
-                bool isHeavy() { return false; };
-                bool isStep() { return false; };
-        };
-
-        class BlockOpen : public Indicator
-        {
-            public:
-                bool isStep() { return false; };
-        };
-
-        class BlockClose : public Indicator
-        {
-            public:
-                bool isStep() { return false; };
-        };
-
-        class StaticOpen : public BlockOpen
-        {
-            public:
-                char indicator() { return '['; };
-                bool canBeHeavy() { return true; };
-                bool isHeavy();
-
-            private:
-                bool heavy;
-        };
-
-        class StaticClose : public BlockClose
-        {
-            public:
-                char indicator() { return ']'; };
-                bool canBeHeavy() { return true; };
-                bool isHeavy();
-
-            private:
-                bool heavy;
-        };
-
-        class DynamicOpen : public BlockOpen
-        {
-            public:
-                char indicator() { return '('; };
-                bool canBeHeavy() { return true; };
-                bool isHeavy();
-
-            private:
-                bool heavy;
-        };
-
-        class DynamicClose : public BlockClose
-        {
-            public:
-                char indicator() { return ')'; };
-                bool canBeHeavy() { return true; };
-                bool isHeavy();
-
-            private:
-                bool heavy;
-        };
-
-        class Block 
-        {
-            public:
-                // Both should be either heavy or not!
-                BlockOpen open;
-                BlockClose close;
-        };
-
-        class StaticBlock : public Block
-        {
-        };
-
-        class DynamicBlock : public Block
-        {
-        };
-
-        class Rule  // Grammar
-        {
-            public:
-                Indicator& matchingIndicator;
-                list<Indicator> followers;
-        };
-        */
 
         class Parser
         {
             public:
                 Parser();
 
-                void parseStream(std::istream& inputStream);
+                void parseStream(std::istream& inputStream)
+                {
+                    std::istream_iterator<int> 
+                }
 
             private:
+                Path* _atPath;
         };
     }
 }
